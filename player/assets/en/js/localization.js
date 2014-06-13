a034f673c4f11dbfec5374b9ec34f990ef45e82d
Gershon Fiedler <gffilm@gmail.com>
2014-06-13 11:21:38 -0700

    Added tabIndex to control bar elements

--- player/assets/en/js/localization.js
+++ player/assets/en/js/localization.js
@@ -5,25 +5,5 @@
     "header":"Welcome",
     "subHeader":"Please wait while the course loads.....",
     "footer":"Company logo"
-  },
-  "playerContent": {
-    "id": "player",
-    "class": "jp-player",
-    "buttons":[
-        {"button": true, "id": "menu",  "class": "button", "html":"home"},
-        {"button": true, "id": "cc",    "class": "button", "html":"transcript"},
-        {"button": true, "id": "mute",  "class": "button", "html":"mute"},
-        {"button": true, "id": "title", "class": "title", "html":"course title"},
-        {"button": true, "id": "play",  "class": "button", "html":"play"},
-        {"button": true, "id": "pause", "class": "button", "html":"pause"},
-        {"button": true, "id": "next",  "class": "button", "html":"next"}
-      ]
-  },
-  "playerCss": {
-    "buttonColor":"#5f5f5f",
-    "buttonBgColor":"#000",
-    "buttonHoverColor":"#ccc",
-    "buttonBgHoverColor":"#222",
-    "progressBarColor":"#a58306"
   }
 }
--- player/assets/global/js/config.js
+++ player/assets/global/js/config.js
@@ -12,5 +12,25 @@
     "style": "player",
     "styleContent": "playerCss",
     "layoutContent": "playerContent"
+  },
+  "playerContent": {
+    "id": "player",
+    "class": "jp-player",
+    "buttons":[
+        {"button": true, "id": "menu", "html":"home", "tabIndex": 1},
+        {"button": true, "id": "cc", "html":"transcript", "tabIndex": 2},
+        {"button": true, "id": "mute", "html":"mute", "tabIndex": 3},
+        {"button": false, "id": "title",  "class": "title", "html":"course title", "tabIndex": 4},
+        {"button": true, "id": "play", "html":"play", "tabIndex": 5},
+        {"button": true, "id": "pause", "html":"pause", "tabIndex": 6},
+        {"button": true, "id": "next", "html":"next", "tabIndex": 7}
+      ]
+  },
+  "playerCss": {
+    "buttonColor":"#5f5f5f",
+    "buttonBgColor":"#000",
+    "buttonHoverColor":"#ccc",
+    "buttonBgHoverColor":"#222",
+    "progressBarColor":"#a58306"
   }
 }
