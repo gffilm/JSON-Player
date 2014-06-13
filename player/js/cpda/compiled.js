80ddff37f1432973e991dfa20f998fc321f4f296
Gershon Fiedler <gffilm@gmail.com>
2014-06-12 17:35:33 -0700

    Made the engine loader object oriented

--- player/js/cpda/compiled.js
+++ player/js/cpda/compiled.js
@@ -12,62 +12,72 @@ jp.PRODUCTION = false;
 
 
 /*
- * Set the number of asset paths
- * @type {boolean}
- */
-jp.assetPathsLoaded = 0;
-
-
-/*
- * Set the jsonData
- * @type {Object}
- */
-jp.jsonData = [];
-
-
-/*
- * The Required libraries dependent on production mode
- */
-if (jp.PRODUCTION) {
-  jp.libraries = [
-  'js/lib/min/modernizr.min.js',
-  'js/lib/min/jquery-1.11.1-min.js',
-  'js/lib/min/dust-full-0.3.0.min.js'
-  ];
-} else {
-  jp.libraries = [
-  'js/lib/modernizr.js',
-  'js/lib/jquery-1.11.1.js',
-  'js/lib/dust-full-0.3.0.js'
-  ];
-};
-
-/*
- * The APIs to be loaded
- */
-jp.libraryAPIs = ['Modernizr', 'jQuery', 'dust'];
-
-
-/*
- * The Error objects wih a code and detail
- */
-jp.ErrorCodes = {
-  'libraryLoad': {'code': 'loader-001', 'detail': 'Error loading libraries'},
-  'jsonLoad': {'code': 'loader-002', 'detail': 'Error loading json files'},
-  'dustError': {'code': 'dust-001', 'detail': 'Error parsing dust file'},
-  'dustMarkup': {'code': 'dust-002', 'detail': 'Dust markup missing'},
-  'layoutMissing': {'code': 'dust-003', 'detail': 'Dust layout missing'},
-  'layoutContentMissing': {'code': 'dust-004', 'detail': 'Dust layout content missing'}
-}
-
-
-/*
  * Avoids errors if there is not a console
 */
 if (!window.console) {
   console = {};
   console.log = function() {};
 }
+
+
+/**
+ * Pure-JS implementation of jp.bind.
+ * @param {Function} fn A function to partially apply.
+ * @param {Object|undefined} selfObj Specifies the object
+ * @param {...*} var_args Additional arguments
+ * @return {!Function} A partially-applied form of the function bind()
+ * @private
+ */
+jp.bindJs_ = function(fn, selfObj, var_args) {
+  var context = selfObj || jp.global;
+  if (arguments.length > 2) {
+    var boundArgs = Array.prototype.slice.call(arguments, 2);
+    return function() {
+      // Prepend the bound arguments to the current arguments.
+      var newArgs = Array.prototype.slice.call(arguments);
+      Array.prototype.unshift.apply(newArgs, boundArgs);
+      return fn.apply(context, newArgs);
+    };
+
+  } else {
+    return function() {
+      return fn.apply(context, arguments);
+    };
+  }
+};
+
+
+/**
+ * A native implementation of jp.bind.
+ * @param {Function} fn A function to partially apply.
+ * @param {Object|undefined} selfObj the object itself.
+ * @param {...*} var_args Additional arguments
+ * @return {!Function} the function to bind
+ * @private
+ */
+jp.bindNative_ = function(fn, selfObj, var_args) {
+  return fn.call.apply(fn.bind, arguments);
+};
+
+
+/**
+ * @param {Function} fn A function to partially apply.
+ * @param {Object|undefined} selfObj Specifies the object
+ * @param {...*} var_args Additional arguments
+ * @return {!Function} the function bind()
+ */
+jp.bind = function(fn, selfObj, var_args) {
+  if (Function.prototype.bind &&
+      Function.prototype.bind.toString().indexOf('native code') != -1) {
+    jp.bind = jp.bindNative_;
+  } else {
+    jp.bind = jp.bindJs_;
+  }
+  return jp.bind.apply(null, arguments);
+};
+
+
+
 
 
 /*
@@ -83,6 +93,7 @@ jp.error = function(error, optInfo) {
   }
 };
 
+
 /*
  * Logs an entry
  * @param {string} name the logging name.
@@ -93,15 +104,97 @@ jp.log = function(name, value) {
 };
 
 
+/*
+ * The Error objects wih a code and detail
+ */
+jp.ErrorCodes = {
+  'libraryLoad': {'code': 'loader-001', 'detail': 'Error loading libraries'},
+  'jsonLoad': {'code': 'loader-002', 'detail': 'Error loading json files'},
+  'dustError': {'code': 'dust-001', 'detail': 'Error parsing dust file'},
+  'dustMarkup': {'code': 'dust-002', 'detail': 'Dust markup missing'},
+  'layoutMissing': {'code': 'dust-003', 'detail': 'Dust layout missing'},
+  'layoutContentMissing': {'code': 'dust-004', 'detail': 'Dust layout content missing'}
+};
+
+
+jp.events = {
+  layoutLoad: 'layoutLoad'
+};
+
+
+/*
+ * The class to setup and load the engine
+ * @constructor
+ */
+ jp.engine = function() {
+
+  /*
+ * Set the number of asset paths
+ * @type {boolean}
+ */
+  this.assetPathsLoaded_ = 0;
+
+
+  /*
+ * The Required libraries dependent on production mode
+ */
+  if (jp.PRODUCTION) {
+    this.libraries_ = [
+    'js/lib/min/modernizr.min.js',
+    'js/lib/min/jquery-1.11.1-min.js',
+    'js/lib/min/dust-full-0.3.0.min.js'
+    ];
+  } else {
+    this.libraries_ = [
+    'js/lib/modernizr.js',
+    'js/lib/jquery-1.11.1.js',
+    'js/lib/dust-full-0.3.0.js'
+    ];
+  }
+
+  /*
+ * The APIs to verify
+ */
+  this.libraryAPIs_ = ['Modernizr', 'jQuery', 'dust'];
+
+
+  /*
+ * Set the jsonData
+ * @type {Object}
+ */
+  this.jsonData_ = {};
+
+ /*
+ * The layout type
+ * @type {string}
+ */
+  this.layout_;
+
+ /*
+ * The layout type
+ * @type {Object}
+ */
+  this.layoutContent_;
+ };
+
+/*
+ * The startup function called from the index file
+ * @public
+*/
+ jp.startup = function() {
+  jp.engine = new jp.engine();
+  jp.engine.load();
+};
 
 /*
  * Loads libraries using yepNope
 */
-jp.load = function() {
+jp.engine.prototype.load = function() {
+  var that = this;
   yepnope({
-    load: jp.libraries,
+    load: this.libraries_,
     complete: function() {
-      jp.init();
+      that.init();
     }
   });
 };
@@ -110,13 +203,13 @@ jp.load = function() {
 /*
  * Initializer after libraries are loaded
 */
-jp.init = function() {
-  var paths = jp.getAssetPaths();
+jp.engine.prototype.init = function() {
+  var paths = this.getAssetPaths();
   // set the total number of asset paths
-  jp.totalAssetPaths = paths.length;
+  this.totalAssetPaths_ = paths.length;
   // Verify the apis are set and then load the json files
-  if (jp.verifyAPIs()) {
-    jp.loadJSON(paths, jp.handleJSONLoad);
+  if (this.verifyAPIs()) {
+    this.loadJSON(paths, jp.bind(this.handleJSONLoad, this));
   } else {
     jp.error(jp.ErrorCodes['libraryLoad']);
   }
@@ -127,10 +220,11 @@ jp.init = function() {
  * Gets the asset paths
  * @return {Array} the asset paths.
 */
-jp.getAssetPaths = function() {
+jp.engine.prototype.getAssetPaths = function() {
   // Get the set language
-  var language = jp.utility.getUrlParams('lang') || 'en',
-      courseId = jp.utility.getUrlParams('id'),
+  var utility = new jp.utility(),
+      language = utility.getUrlParams('lang') || 'en',
+      courseId = utility.getUrlParams('id'),
       assetPaths;
   //The Asset paths to load
   assetPaths = [
@@ -156,10 +250,10 @@ jp.getAssetPaths = function() {
  * Verifies libraries
  * @return {Boolean} are all libraries available?
 */
-jp.verifyAPIs = function() {
+jp.engine.prototype.verifyAPIs = function() {
   verified = true;
-  for (i in jp.libraryAPIs) {
-    if (!window[jp.libraryAPIs[i]]) {
+  for (i in this.libraryAPIs_) {
+    if (!window[this.libraryAPIs_[i]]) {
       verified = false;
     }
   }
@@ -172,8 +266,8 @@ jp.verifyAPIs = function() {
  * @param {Array} paths we take the first path to load.
  * @param {Function} callback the callback function.
 */
-jp.loadJSON = function(paths, callback) {
-  var path = paths[0], request;
+jp.engine.prototype.loadJSON = function(paths, callback) {
+  var path = paths[0], request, that = this;
 
   if (!path) {
     return;
@@ -185,10 +279,10 @@ jp.loadJSON = function(paths, callback) {
     // run the callback with the data
     callback(data);
     // reload this function until there are no more paths
-    jp.loadJSON(paths, callback);
+    that.loadJSON(paths, callback);
   }).fail(function(data, response) {
     jp.error(jp.ErrorCodes['jsonLoad'], path + ' ' + response);
-    jp.loadJSON(paths, callback);
+    that.loadJSON(paths, callback);
     callback({});
   });
 };
@@ -198,23 +292,30 @@ jp.loadJSON = function(paths, callback) {
  * Handles json paths loaded
  * @param {Object} data the json data.
 */
-jp.handleJSONLoad = function(data) {
+jp.engine.prototype.handleJSONLoad = function(data) {
   // Increment the number of paths loaded
-  jp.assetPathsLoaded++;
+  this.assetPathsLoaded_++;
   // Extend the jsonData object
-  $.extend(jp.jsonData, data);
+  $.extend(this.jsonData_, data);
   // Determine if we have loaded all paths
-  if (jp.totalAssetPaths === jp.assetPathsLoaded) {
-    jp.activate();
+  if (this.totalAssetPaths_ === this.assetPathsLoaded_) {
+    this.activate();
   }
 };
 
 
-jp.activate = function() {
-  jp.welcome();
+jp.engine.prototype.activate = function() {
+  var layout = new jp.layouts, eventHandler;
+
+  eventHandler = function(evt) {
+    layout.render('myLayout', 'body');
+  }.bind(this);
+
+  this.welcome();
   // Load the layouts
-  if (jp.getLayout()) {
-    jp.layouts.load('assets/global/layouts/' + jp.getLayout() + '.html');
+  if (this.getLayout()) {
+    $(layout).bind(jp.events.layoutLoad, eventHandler);
+    layout.load('assets/global/layouts/' + this.getLayout() + '.html');
   } else {
     cpda.error(jp.ErrorCodes['layoutMissing']);
   }
@@ -225,8 +326,8 @@ jp.activate = function() {
  * Sets the layout
  * @param {string} layout the layout name.
 */
-jp.setLayout = function(layout) {
-  jp.layout = layout;
+jp.engine.prototype.setLayout = function(layout) {
+  this.layout_ = layout;
 };
 
 
@@ -234,8 +335,17 @@ jp.setLayout = function(layout) {
  * Gets the layout
  * @return {string} the layout name.
 */
-jp.getLayout = function() {
-  return jp.layout;
+jp.engine.prototype.getLayout = function() {
+  return this.layout_;
+};
+
+
+/*
+ * Gets the jsondata
+ * @return {string} the layout name.
+*/
+jp.engine.prototype.getJsonData = function() {
+  return this.jsonData_;
 };
 
 
@@ -243,8 +353,8 @@ jp.getLayout = function() {
  * Sets the layout content
  * @param {string} layoutContent the layout name.
 */
-jp.setLayoutContent = function(layoutContent) {
-  jp.layoutContent = layoutContent;
+jp.engine.prototype.setLayoutContent = function(layoutContent) {
+  this.layoutContent_ = layoutContent;
 };
 
 
@@ -252,25 +362,27 @@ jp.setLayoutContent = function(layoutContent) {
  * Gets the layout content
  * @return {string} the layout name.
 */
-jp.getLayoutContent = function() {
-  return jp.layoutContent;
+jp.engine.prototype.getLayoutContent = function() {
+  return this.layoutContent_;
 };
 
 
 /*
  * Welcome message as the course loads
 */
-jp.welcome = function() {
+jp.engine.prototype.welcome = function() {
   var div,
-      title = jp.utility.getJsonData(['course', 'title'], jp.jsonData),
-      className = jp.utility.getJsonData(['loader', 'class'], jp.jsonData),
-      id = jp.utility.getJsonData(['loader', 'id'], jp.jsonData),
-      layout = jp.utility.getJsonData(['loader', 'layout'], jp.jsonData);
-      layoutContent = jp.utility.getJsonData(['loader', 'layoutContent'], jp.jsonData);
+      util = new jp.utility(),
+      jsonData = this.getJsonData();
+      title = util.findJsonData(['course', 'title'], jsonData),
+      className = util.findJsonData(['loader', 'class'], jsonData),
+      id = util.findJsonData(['loader', 'id'], jsonData),
+      layout = util.findJsonData(['loader', 'layout'], jsonData);
+      layoutContent = util.findJsonData(['loader', 'layoutContent'], jsonData);
 
-  jp.setLayout(layout);
-  jp.setLayoutContent(layoutContent);
-  div = jp.ui.createElement('div', id, className);
+  this.setLayout(layout);
+  this.setLayoutContent(layoutContent);
+  div = new jp.ui().createElement('div', id, className);
   $('body').append(div);
   document.title = title;
   jp.welcomeDiv = div;
@@ -280,7 +392,7 @@ jp.welcome = function() {
 /*
  * Welcome message as the course loads
 */
-jp.removeWelcome = function() {
+jp.engine.prototype.removeWelcome = function() {
   if (jp.welcomeDiv) {
     jp.welcomeDiv.remove();
   }
@@ -297,7 +409,7 @@ jp.ui = function() {};
  * @param {string} id the element id.
  * @param {string} innerHTML the innerHtml to set.
 */
-jp.ui.setHtmlById = function(id, innerHTML) {
+jp.ui.prototype.setHtmlById = function(id, innerHTML) {
   $('#' + id).html(innerHTML);
 };
 
@@ -310,7 +422,7 @@ jp.ui.setHtmlById = function(id, innerHTML) {
  * @param {string=} innerHTML the innerHtml to set.
  * @return {Element} the element created
 */
-jp.ui.createElement = function(type, id, className, innerHTML) {
+jp.ui.prototype.createElement = function(type, id, className, innerHTML) {
   return jQuery('<' +type + '>', {
     'id': id,
     'text': innerHTML,
@@ -330,7 +442,7 @@ jp.utility = function() {};
  * @param {string} param the uri param to try for.
  * @return {string} the param or null.
 */
-jp.utility.getUrlParams = function(param) {
+jp.utility.prototype.getUrlParams = function(param) {
   return decodeURIComponent(
     (new RegExp('[?|&]' + param + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) ||
       [,""])[1].replace(/\+/g, '%20')) || null;
@@ -338,12 +450,12 @@ jp.utility.getUrlParams = function(param) {
 };
 
 /*
- * Gets json data.
+ * Finds json data from a haystack.
  * @param {Array} needles the data to look for.
  * @param {Object} haystack the data to search through.
  * @return {*} the data or null.
 */
-jp.utility.getJsonData = function(needles, haystack) {
+jp.utility.prototype.findJsonData = function(needles, haystack) {
   var i, needle, found = false;
   for (i in needles) {
     needle = needles[i];
@@ -359,23 +471,21 @@ jp.utility.getJsonData = function(needles, haystack) {
 /*
  * The Layouts class
 */
-jp.layouts = {};
+jp.layouts = function() {};
 
 
 /*
  * Gets a dust layout based on a path and renders it once loaded.
  * @param {string} path the uri of the layout to load.
- * @param {Element=} optParent the optional parent element to load the template into.
 */
-jp.layouts.load = function(path, optParent) {
+jp.layouts.prototype.load = function(path) {
   var compiled,
       name = 'myLayout',
       callback = function(data) {
         compiled = dust.compile(data, name);
         dust.loadSource(compiled);
-        jp.layouts.render(name, optParent);
-      };
-  optParent = optParent || 'body';
+        $(this).trigger(jp.events.layoutLoad);
+      }.bind(this);
   $.get(path, callback);
 }
 
@@ -385,14 +495,15 @@ jp.layouts.load = function(path, optParent) {
  * @param {string} name the layout name.
  * @param {Element} parent the parent element to load the template into.
 */
-jp.layouts.render = function(name, parent) {
-  var jsonData = jp.jsonData[jp.getLayoutContent()],
+jp.layouts.prototype.render = function(name, parent) {
+  var data = jp.engine.getJsonData(),
+      layoutContent = jp.engine.getLayoutContent(),
+      jsonData = data[layoutContent],
       callback = function(error, info) {
         if (error) {
           jp.error(jp.ErrorCodes['dustError'], error);
           return;
         }
-      jp.removeWelcome();
       $(parent).append(info);
     };
   if (!jsonData) {
