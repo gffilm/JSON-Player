492872644e5f31fc8413dd94f0e711bcc945b4e8
Gershon Fiedler <gffilm@gmail.com>
2014-06-16 15:09:38 -0700

    Clean up

--- player/js/jp/dustHelpers.js
+++ player/js/jp/dustHelpers.js
@@ -2,24 +2,22 @@
  * The Dust Helpers class
 */
 jp.dustHelpers = function() {
-  	/**
+
+  /**
   * Macro for getting a defined variable (could be anything: size, color, etc.)
   * @param {object} params the variables placed in the dss/dsx files
   * @return {chunk} compiled dss of the defined item
    */
   this.define = function(chunk, context, bodies, params) {
-    var dss = params.name,
-        rgba = params.rgba,
-        dsx = context.get(dss),
-        brand = context.get(dsx),
-        render;
+    var cssName = params.name,
+        cssValue = context.get(cssName);
+        brandedValue = context.get(cssValue);
     
     // If no brand exists, add a warning and fallback to the dsx value
-    if (!brand && dsx) {
-      brand = dsx;
+    if (!brandedValue && cssValue) {
+      brandedValue = cssValue;
     }
 
-    render = brand;
-    return chunk.write(render);
+    return chunk.write(brandedValue);
   }
-}
+};
